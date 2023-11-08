d4f5d71f285306edb11e5b0022a4fe7e43c35655
Reksa1903 <masabidreksa@gmail.com>
2023-11-08 14:22:31 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*============================ typing animation ===================*/
+var typed = new Typed(".typing", {
+    strings: ["", "Mahasiswa Teknik Informatika", "Software Development", "Front-end Development", "Back-end Developmnet"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
